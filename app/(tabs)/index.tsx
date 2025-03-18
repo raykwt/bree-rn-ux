19ce44b92ad7398cb04880b1d9c36a3f8e3e86ba
raykwt <raykwtsang.ca@gmail.com>
2025-03-19 07:51:11 +0800

    feat: reinstall packages

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,117 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {
+  Text,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+} from "react-native";
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const lower = new RegExp("(?=.*[a-z])");
+const upper = new RegExp("(?=.*[A-Z])");
+const number = new RegExp("(?=.*[0-9])");
+const special = new RegExp("(?=.*[!@#$%^&*)+-])");
 
-export default function HomeScreen() {
+const passwordSchema = z
+  .string({ required_error: "Your password does not fulfill the complexity." })
+  .refine((data) => lower.test(data), {
+    path: ["lower"],
+  })
+  .refine((data) => upper.test(data), {
+    path: ["upper"],
+  })
+  .refine((data) => number.test(data), {
+    path: ["number"],
+  })
+  .refine((data) => special.test(data), {
+    path: ["special"],
+  })
+  .refine((data) => data.length >= 8 && data.length <= 20, {
+    path: ["length"],
+  });
+
+export const createPasswordSchema = z
+  .object({
+    password: passwordSchema,
+    confirmPassword: z.string({}),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+  });
+const CreatePasswordScreen = () => {
+  const {
+    watch,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(createPasswordSchema),
+    defaultValues: {
+      password: "",
+      confirmPassword: "",
+    },
+    mode: "all",
+    reValidateMode: "onChange",
+  });
+  const password = watch("password");
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <Text style={styles.text}> Set your password </Text>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              // secureTextEntry={true}
+              onBlur={onBlur}
+              onChange={onChange}
+              value={value}
+              placeholder="@Password1"
+              onChangeText={(text: String) => {
+                onChange(text);
+              }}
+            />
+          )}
+          name="password"
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              // secureTextEntry={true}
+              onBlur={onBlur}
+              onChange={onChange}
+              value={value}
+              placeholder="@Password1"
+              onChangeText={(text: String) => {
+                onChange(text);
+              }}
+            />
+          )}
+          name="confirmPassword"
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  text: {
+    paddingHorizontal: 12,
+    fontSize: 32,
   },
 });
+
+export default CreatePasswordScreen;
